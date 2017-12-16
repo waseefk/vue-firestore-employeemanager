8b7e1eb9d4de50051d49ec3baf17dca4a5bc87a3
Waseef <waseef.job@gmail.com>
2017-12-16 22:35:52 +0530

    Initial Commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default { 
+  apiKey: "AIzaSyCCJYFGo4k4HRRs6qGzPOVBL_Swo72RXVA",
+  authDomain: "vuefs-prod-fb530.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-fb530.firebaseio.com",
+  projectId: "vuefs-prod-fb530",
+  storageBucket: "vuefs-prod-fb530.appspot.com",
+  messagingSenderId: "206277284437"
+}
